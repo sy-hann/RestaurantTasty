7ac93e45e90afba65edab4ce312dc9e0433b3454
thecoder523 <75168745+thecoder523@users.noreply.github.com>
2021-02-01 21:26:12 +0900

    Light / Dark mode

--- app/js/main.js
+++ app/js/main.js
@@ -45,6 +45,7 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive)
 
+
 // Change Background Header
 function scrollHeader() {
     const nav = document.getElementById('header')
@@ -65,3 +66,39 @@ function scrollTop(){
 }
 
 window.addEventListener('scroll', scrollTop);
+
+
+// =============================================================
+// Dark Light Theme
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Obtain the current theme that the interface has by validating the dark=theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+
+// Validate if the user previously chose a topic
+if(selectedTheme){
+    // If the validation is fulfilled, we ask what the issue was to know if we activated
+    // or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the btn
+themeButton.addEventListener('click', ()=>{
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    // Save the theme and the current icon that the user chose
+    // Even after reloading
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
